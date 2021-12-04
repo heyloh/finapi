a6d515651351e3c14b23438529e476c1cb83f4d2
Lohana Torres <lt.leopoldo@gmail.com>
2021-12-03 23:19:37 -0300

    feat: create basic express app listening 3333

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+  return response.json({
+    message: "Hello Ignite",
+  });
+});
+
+const port = 3333;
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
+});
