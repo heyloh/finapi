2f4a7bac89dc4669f5de7e88f6727ca8f04d7e50
Lohana Torres <lt.leopoldo@gmail.com>
2021-12-03 23:46:33 -0300

    feat: change route

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/courses", (request, response) => {
+app.get("/", (request, response) => {
   return response.json({
     message: "Hello Ignite",
   });
