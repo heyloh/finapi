866b9d3a8248bfc24d9dd2ea2cf1c9b5e65951f3
Lohana Torres <lt.leopoldo@gmail.com>
2021-12-05 12:30:13 -0300

    feat: route for handling get customer statement

--- src/index.js
+++ src/index.js
@@ -28,6 +28,18 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found." });
+  }
+
+  return response.json(customer.statement);
+});
+
 const port = 3333;
 app.listen(port, () => {
   console.log(`🚀 Server running on port ${port}`);
