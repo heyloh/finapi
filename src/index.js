8d57f9ff6090ac8f8edb76acbf266233965ee369
Lohana Torres <lt.leopoldo@gmail.com>
2021-12-05 15:05:15 -0300

    feat: function that calculates balance of customer

--- src/index.js
+++ src/index.js
@@ -22,6 +22,18 @@ function verifyIfExistsAccountWithCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((accumulator, operation) => {
+    if (operation.type === "credit") {
+      return accumulator + operation.amount;
+    } else {
+      return accumulator - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
